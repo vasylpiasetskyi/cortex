c4af3aaa69de7c23eec99126320b615ea6ab2290
Vasyl Piasetskyi <vasylpiasetskij@gmail.com>
2026-06-27 10:20:58 +0200

    fix: allow CORS from 127.0.0.1:3000 in addition to localhost:3000

--- app/main.py
+++ app/main.py
@@ -34,7 +34,7 @@ app = FastAPI(title="Cortex", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
